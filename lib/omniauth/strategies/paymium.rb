697d2bcf4a30920640b095e7c07f389a00bda078
Pierre Michard <pmich35@gmail.com>
2017-10-03 12:44:32 +0200

    style

--- lib/omniauth/strategies/paymium.rb
+++ lib/omniauth/strategies/paymium.rb
@@ -6,25 +6,25 @@ module OmniAuth
     class Paymium < OmniAuth::Strategies::OAuth2
       option :name, 'paymium'
 
-      option :client_options, {
-        :site => 'https://www.paymium.com/',
-        :authorize_url => 'https://www.paymium.com/api/oauth/authorize',
-        :token_url => 'https://www.paymium.com/api/oauth/token'
-      }
+      option :client_options,
+        site: 'https://www.paymium.com/',
+        authorize_url: 'https://www.paymium.com/api/oauth/authorize',
+        token_url: 'https://www.paymium.com/api/oauth/token'
 
-      option :access_token_options, {
-        :mode => :query
-      }
 
-      option :authorize_params, {
-        :scope => 'admin'
-      }
+      option :access_token_options,
+        mode: :query
+
+
+      option :authorize_params,
+        scope: 'admin'
+
 
       def build_access_token
         token_params = {
-          :redirect_uri => callback_url.split('?').first,
-          :client_id => client.id,
-          :client_secret => client.secret
+            redirect_uri: callback_url.split('?').first,
+            client_id: client.id,
+            client_secret: client.secret
         }
         verifier = request.params['code']
         client.auth_code.get_token(verifier, token_params)
